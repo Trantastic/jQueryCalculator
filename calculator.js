ca94f7243650745a0285a3656171b8a11bf42e55
Yvette <yvettetran@gmail.com>
2018-05-13 10:53:15 -0700

    created clear and calculate functions

--- calculator.js
+++ calculator.js
@@ -2,9 +2,11 @@ $(document).ready(() => {
 	var firstNum = "";
 	var secondNum = "";
 	var operator = "";
+	var result;
 	var isOpChosen;
 	var calculated;
 
+	// User input number listener
 	$(".number").on("click", function() {
 		if(calculated){
 			return;
@@ -19,17 +21,48 @@ $(document).ready(() => {
 		}
 	});
 
+	// User input operator listener
 	$(".operator").on("click", function() {
-		if(calculated){
-			return;
-		}
-		else if(firstNum !== ""){
+		if(!calculated || firstNum !== ""){
 			isOpChosen = true;
 			operator = this.value;
 			$("#operator").html($(this).text());
 		}
 	});
 
+	function runCalcu() {
+		switch(operator) {
+			case "plus":
+				result = firstNum + secondNum;
+				break;
+			case "minus":
+				result = firstNum - secondNum;
+				break;
+			case "divide":
+				result = firstNum / secondNum;
+				break;
+			case "times":
+				result = firstNum * secondNum;
+				break;
+			case "power":
+				result = Math.pow(parseInt(firstNum), parseInt(secondNum));
+				break;
+		}
+	}
+
+	function clear() {
+		var firstNum = "";
+		var secondNum = "";
+		var operator = "";
+		var result;
+		var isOpChosen;
+		var calculated;
+
+		$("#first-number").empty();
+		$("#second-number-number").empty();
+		$("#operator").empty();
+		$("#result").empty();
+	}
 
 
 });
